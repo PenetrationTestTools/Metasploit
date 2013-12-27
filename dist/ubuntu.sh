c7d57d0495299cf39b8691e621181da5c73ada53
Naoya Sawada <naoya@tuntunkun.com>
2013-12-27 19:02:22 +0900

    add ubuntu script

--- dist/ubuntu.sh
+++ dist/ubuntu.sh
@@ -5,6 +5,15 @@
 # curlのインストール
 #
 sudo apt-get install -y curl
+sudo apt-get install build-essential bison libc6-dev ncurses-dev
+sudo apt-get install libffi-dev
+sudo apt-get install openssl libssl-dev libyaml-dev
+sudo apt-get install libreadline6 libreadline6-dev
+sudo apt-get install automake autoconf pkg-config
+sudo apt-get install git-core subversion
+sudo apt-get install zlib1g zlib1g-dev
+sudo apt-get install sqlite3 libsqlite3-dev libgdbm-dev
+sudo apt-get install libxml2-dev libxslt-dev
 
 # rvmのインストール
 if [ ! -e ~/.rvm ]; then
